e7a1e703409c88eefde3e75a9a27f577c1b3ab78
Peter Isaac <pisaac.ozflux@gmail.com>
2014-12-12 12:52:05 +1100

    Initial commit of Peter Isaac's iPython notebook files.

---
+++ basics.py
@@ -0,0 +1,13 @@
+import csv
+import sys
+sys.path.append('../scripts')
+import constants as c
+import datetime
+import matplotlib.pyplot as plt
+import meteorologicalfunctions as mf
+import netCDF4
+import numpy
+import qcio
+import qcts
+import qcutils
+import scipy
