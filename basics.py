af1390ea730084962fc02f25f2e48e223d665c1e
Peter Isaac <pisaac.ozflux@gmail.com>
2015-06-24 09:01:20 +1000

    Mods to some and some new ones.

--- basics.py
+++ basics.py
@@ -3,10 +3,13 @@ import sys
 sys.path.append('../scripts')
 import constants as c
 import datetime
+import dateutil
 import matplotlib.pyplot as plt
 import meteorologicalfunctions as mf
 import netCDF4
 import numpy
+import qcck
+import qcgf
 import qcio
 import qcts
 import qcutils
