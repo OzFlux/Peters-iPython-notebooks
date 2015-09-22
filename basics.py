3f0f910aedc4b0e0e16db4b2b13d7cc7962a2305
Peter Isaac <pisaac.ozflux@gmail.com>
2015-09-22 10:22:25 +1000

    Long time, no commits ... Commit of all IPython notebooks on 22/9/2015.

--- basics.py
+++ basics.py
@@ -1,16 +1,21 @@
 import csv
 import sys
-sys.path.append('../scripts')
-import constants as c
 import datetime
 import dateutil
 import matplotlib.pyplot as plt
-import meteorologicalfunctions as mf
 import netCDF4
 import numpy
+import os
+import scipy
+
+os.chdir('../')
+#sys.path.append('../scripts')
+sys.path.append('scripts')
+import constants as c
+import meteorologicalfunctions as mf
 import qcck
 import qcgf
 import qcio
+import qcrp
 import qcts
 import qcutils
-import scipy
