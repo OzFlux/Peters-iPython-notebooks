4c64be80b941afe41c77dba24ee9ea59907420c9
Peter Isaac <pisaac.ozflux@gmail.com>
2015-12-06 11:43:31 +1100

    Staging commit from laptop.

--- basics.py
+++ basics.py
@@ -7,15 +7,17 @@ import netCDF4
 import numpy
 import os
 import scipy
+import time
 
 os.chdir('../')
-#sys.path.append('../scripts')
 sys.path.append('scripts')
 import constants as c
 import meteorologicalfunctions as mf
 import qcck
+import qcfunc
 import qcgf
 import qcio
+import qcplot
 import qcrp
 import qcts
 import qcutils
